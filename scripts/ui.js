b32e8c3354e5d30d760e7f8f4d086ce6beeec2cc
Sue-svbd <susannacapacchione@gmail.com>
2020-01-17 19:52:10 +0100

    Chat working

---
+++ scripts/ui.js
@@ -0,0 +1,18 @@
+class ChatUI {
+  constructor(list) {
+    this.list = list;
+  }
+  clear() {
+    this.list.innerHTML = "";
+  }
+  render(data) {
+    const html = `
+      <li class="list-group-item">
+        <span class="username">${data.username}</span>
+        <span class="message">${data.message}</span>
+        <div class="time">${data.created_at.toDate()}</span>
+      </li>
+    `;
+    this.list.innerHTML += html;
+  }
+}
